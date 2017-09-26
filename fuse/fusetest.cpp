41db3a7725e8e8143c426361f5ec31d12fa75289
Jim Lembke <lembkej@purdue.edu>
2017-09-26 16:29:44 -0400

    changes

--- fuse/fusetest.cpp
+++ fuse/fusetest.cpp
@@ -15,17 +15,54 @@ char* target_dir = NULL;
 int thread_count = 0;
 int file_count = 0;
 
-char write_buffer[128];
-
-void* thread_main(void* arg)
+void* write_main(void* arg)
 {
+	char write_buffer[128];
 	std::string filepath = target_dir;
 	filepath += "/testfile";
 	filepath += std::to_string(*(int*)(arg));
 	for(int i = 0; i < file_count; i++) {
 		int fd = open((filepath + std::to_string(i)).c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 		if(fd > 0) {
-			write(fd, write_buffer, sizeof(write_buffer));
+			int ret = write(fd, write_buffer, sizeof(write_buffer));
+			if(ret == -1) {
+				fprintf(stderr, "Unable to write to %s: %s\n", (filepath + std::to_string(i)).c_str(), strerror(errno));
+				exit(1);
+			}
+			if(ret != sizeof(write_buffer)) {
+				fprintf(stderr, "Write to file %s size: %d expected %d\n", (filepath + std::to_string(i)).c_str(), ret, (int)sizeof(write_buffer));
+				exit(1);
+			}
+		} else {
+			fprintf(stderr, "Unable to open %s: %s\n", (filepath + std::to_string(i)).c_str(), strerror(errno));
+			exit(1);
+		}
+		close(fd);
+	}
+	return NULL;
+}
+
+void* read_main(void* arg)
+{
+	char read_buffer[128];
+	std::string filepath = target_dir;
+	filepath += "/testfile";
+	filepath += std::to_string(*(int*)(arg));
+	for(int i = 0; i < file_count; i++) {
+		int fd = open((filepath + std::to_string(i)).c_str(), O_RDONLY);
+		if(fd > 0) {
+			int ret = read(fd, read_buffer, sizeof(read_buffer));
+			if(ret == -1) {
+				fprintf(stderr, "Unable to read to %s: %s\n", (filepath + std::to_string(i)).c_str(), strerror(errno));
+				exit(1);
+			}
+			if(ret != sizeof(read_buffer)) {
+				fprintf(stderr, "Read from file %s size: %d expected %d\n", (filepath + std::to_string(i)).c_str(), ret, (int)sizeof(read_buffer));
+				exit(1);
+			}
+		} else {
+			fprintf(stderr, "Unable to open %s: %s\n", (filepath + std::to_string(i)).c_str(), strerror(errno));
+			exit(1);
 		}
 		close(fd);
 	}
@@ -51,13 +88,13 @@ int main(int argc, char* argv[])
 	uint64_t start_time = 0;
 	uint64_t end_time = 0;
 
-	if(argc != 4) {
-		printf("Usage: fusetest target_dir threads files\n");
+	if(argc != 5) {
+		printf("Usage: fusetest r|w target_dir threads files\n");
 		exit(1);
 	}
-	target_dir = (char*)argv[1];
-	thread_count = atoi(argv[2]);
-	file_count = atoi(argv[3]);
+	target_dir = (char*)argv[2];
+	thread_count = atoi(argv[3]);
+	file_count = atoi(argv[4]);
 
 	if(thread_count <= 0) {
 		printf("threads must be > 0\n");
@@ -67,13 +104,24 @@ int main(int argc, char* argv[])
 		printf("files must be > 0\n");
 		exit(1);
 	}
+	void *(*thread_func)(void*) = NULL;
+	if(argv[1][0] == 'r') {
+		thread_func = read_main;
+	}
+	else if(argv[1][0] == 'w') {
+		thread_func = write_main;
+	} else {
+		printf("invalid test type: %s\n", argv[1]);
+		exit(1);
+	}
+
 	thread_ids = new pthread_t[thread_count];
 	thread_args = new int[thread_count];
 
 	start_time = getTime();
 	for(int i = 0; i < thread_count; i++) {
 		thread_args[i] = i;
-		if(pthread_create(&thread_ids[i], NULL, thread_main, &(thread_args[i])) != 0) {
+		if(pthread_create(&thread_ids[i], NULL, thread_func, &(thread_args[i])) != 0) {
 			printf("Create thread %d failed\n", i);
 			exit(1);
 		}
