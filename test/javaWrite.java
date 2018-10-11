6495ec673c860fdd2d03715c2c820f3c53a0cb66
Jim Lembke <lembkej@purdue.edu>
2018-10-11 08:36:31 -0400

    fix javawrite

---
+++ test/javaWrite.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class javaWrite
+{
+	public static void main(String[] args) throws Throwable
+	{
+		if(args.length != 2) {
+			System.err.println("Usage: javaWrite filename data");
+			System.exit(1);
+		}
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(args[0]));
+		writer.write(args[1], 0, args[1].length());
+		writer.close();
+	}
+}
