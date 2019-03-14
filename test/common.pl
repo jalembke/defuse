d4a944291627f9c65cab87617530e315c6fed3b3
James A Lembke <lembkej@purdue.edu>
2019-03-14 16:34:22 -0700

    updates to test scripts

--- test/common.pl
+++ test/common.pl
@@ -2,6 +2,27 @@
 
 use strict;
 
+use File::Basename;
+use Cwd 'abs_path';
+my $script_dir = dirname(abs_path($0));
+my $src_dir = dirname($script_dir);
+
+my $base_backend_mount = "/mnt";
+my $base_wrapper_mount = "/tmp";
+my $test_dir = "/test";
+
+my $fuse_mount_path = "$base_wrapper_mount/fuse";
+my $direct_mount_path = $base_backend_mount;
+my $usfsal_mount_path = "$base_wrapper_mount/usfsal";
+my $usfsal_backend = "/";
+our $direct_path = "$direct_mount_path$test_dir";
+our $fuse_path = "$fuse_mount_path$direct_path";
+our $usfsal_path = "$usfsal_mount_path$direct_path";
+
+my $fusemount = "$src_dir/fuse/fusexmp_fh";
+my $fusermount = "/bin/fusermount";
+my $usfsal_user_library = "$src_dir/usfs_wrap/libusfs_wrap.so";
+
 sub in_set {
 	my $value = shift;
 	my %set = map {$_ => 1} @_;
@@ -55,6 +76,67 @@ sub system_or_die {
 
 sub system_or_continue {
 	return _do_system(CONTINUE_ON_ERROR, @_);
+}
+
+sub drop_caches {
+	system_or_die("/bin/sync");
+	sleep 1;
+	system_or_die("/bin/echo 1 | /usr/bin/sudo /usr/bin/tee /proc/sys/vm/drop_caches > /dev/null");
+}
+
+sub mount_frontend {
+	my $type = shift;
+	if($type eq "fuse") {
+		system_or_die("$fusemount $fuse_mount_path");
+	} elsif($type eq "fuse_direct") {
+		system_or_die("$fusemount -o direct_io $fuse_mount_path");
+	} elsif($type eq "usfsal") {
+		system_or_die("/usr/bin/sudo /bin/mount -t bopfs -o backend=$usfsal_backend -o library=$usfsal_user_library bopfs $usfsal_mount_path");
+	}
+}
+
+sub umount_frontend {
+	my $type = shift;
+	if($type eq "fuse") {
+		system_or_die("$fusermount -u $fuse_mount_path");
+	} elsif($type eq "fuse_direct") {
+		system_or_die("$fusermount -u $fuse_mount_path");
+	} elsif($type eq "usfsal") {
+		system_or_die("/usr/bin/sudo /bin/umount $usfsal_mount_path");
+	}
+}
+
+sub reset_mounts {
+	my $backend_dev = shift;
+	my $backend_type = shift;
+
+	# Unmount FUSE
+	system_or_continue("$fusermount -u $fuse_mount_path");
+
+	# Unmount usfsal
+	system_or_continue("/usr/bin/sudo /bin/umount $usfsal_mount_path");
+
+	# Reset mount point contents
+	system_or_continue("/bin/rm -rf $fuse_mount_path");
+	system_or_continue("/bin/mkdir -p $fuse_mount_path");
+	system_or_continue("/bin/rm -rf $usfsal_mount_path");
+	system_or_continue("/bin/mkdir -p $usfsal_mount_path");
+
+	# Remove usfsal shared spaces
+	system_or_continue("/bin/rm -rf /tmp/usfsal_*");
+
+	# Reset system mounts for backend file systems
+	system_or_continue("/usr/bin/sudo /bin/umount $direct_mount_path");
+
+	my $mount_options = "";
+	if($backend_type eq "vfat") {
+		$mount_options = "-o rw,uid=\$(id -u),gid=\$(id -g)";
+	} elsif($backend_type eq "tmpfs") {
+		$mount_options = "-t tmpfs -o size=768m"
+	}
+	system_or_die("/usr/bin/sudo /bin/mount $mount_options $backend_dev $direct_mount_path");
+	system_or_die("/usr/bin/sudo /bin/mkdir -p $direct_path");
+	system_or_die("/usr/bin/sudo /bin/chmod 777 $direct_path");
 }
 
 return 1;
--- usfs_wrap/usfs_wrap.c
+++ usfs_wrap/usfs_wrap.c
@@ -23,7 +23,7 @@ int usfs_open(const char* path, int flags, mode_t mode, uint64_t* ret_fh)
 	int ret = 0;
 	char path_to_open[PATH_MAX];
 	snprintf(path_to_open, PATH_MAX, "%s/%s", backend, path);
-	//printf("OPEN: %s\n", path_to_open);
+	//printf("OPEN: %s %o %o\n", path_to_open, flags, mode);
 	int fd = syscall(SYS_open, path_to_open, flags, mode);
 	if (fd > 0) {
 		*ret_fh = (uint64_t)fd;
@@ -56,6 +56,79 @@ int usfs_write(uint64_t fh, const char *buf, size_t size, off_t offset, size_t* 
 	} else {
 		*bytes_written = ret;
 		ret = 0;
+	}
+	return ret;
+}
+
+int usfs_fsync(uint64_t fh, int data_sync)
+{
+	int ret = 0;
+	if(data_sync > 0) {
+		ret = syscall(SYS_fdatasync, fh);
+	} else {
+		ret = syscall(SYS_fsync, fh);
+	}
+	if(-1 == ret) {
+		ret = errno;
+	}
+	return ret;
+}
+
+int usfs_truncate(const char* path, off_t length)
+{
+	int ret =0;
+	char path_to_truncate[PATH_MAX];
+	snprintf(path_to_truncate, PATH_MAX, "%s/%s", backend, path);
+	ret = syscall(SYS_truncate, path_to_truncate, length);
+	if(-1 == ret) {
+		ret = errno;
+	}
+	return ret;
+}
+
+int usfs_ftruncate(uint64_t fh, off_t offset)
+{
+	int ret = syscall(SYS_ftruncate, fh, offset);
+	if(-1 == ret) {
+		ret = errno;
+	}
+	return ret;
+}
+
+int usfs_fgetattr(uint64_t fh, struct stat *stbuf)
+{
+	int ret = syscall(SYS_fstat, (int)fh, stbuf);
+	if(-1 == ret) {
+		ret = errno;
+	}
+	return ret;
+}
+
+int usfs_getattr(const char* path, struct stat *stbuf, int flags)
+{
+	int ret = 0;
+	char path_to_stat[PATH_MAX];
+	snprintf(path_to_stat, PATH_MAX, "%s/%s", backend, path);
+	//printf("GETATTR %s %o\n", path_to_stat, flags);
+	if (flags & AT_SYMLINK_NOFOLLOW) {
+		ret = syscall(SYS_lstat, path_to_stat, stbuf);
+	} else {
+		ret = syscall(SYS_stat, path_to_stat, stbuf);
+	}
+	if(-1 == ret) {
+		ret = errno;
+	}
+	return ret;
+}
+
+int usfs_unlink(const char* path)
+{
+	int ret =0;
+	char path_to_unlink[PATH_MAX];
+	snprintf(path_to_unlink, PATH_MAX, "%s/%s", backend, path);
+	ret = syscall(SYS_unlink, path_to_unlink);
+	if(-1 == ret) {
+		ret = errno;
 	}
 	return ret;
 }
